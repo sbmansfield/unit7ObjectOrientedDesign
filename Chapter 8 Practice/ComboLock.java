8249e96e4d731822bd3cc4618fe89b8a2642067d
sbmansfield <sbmansfield@NN123-HA-14.HS.sd203.org>
2016-01-26 15:04:50 -0600

    1/26

---
+++ Chapter 8 Practice/ComboLock.java
@@ -0,0 +1,29 @@
+public class ComboLock
+{
+    
+    public ComboLock( int secret1, int secret2, int secret3 ) 
+    {
+        
+    }
+    
+    public void reset() 
+    { 
+        
+    }
+    
+    public void turnLeft( int ticks ) 
+    {
+    
+    }
+    
+    public void turnRight( int ticks ) 
+    {
+    
+    }
+    
+    public boolean open() 
+    {
+    
+    }
+    
+}
